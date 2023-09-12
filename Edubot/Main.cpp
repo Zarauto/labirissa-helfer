7480efc5e275de7b2248aa3a4b74a981898ceba7
Zarauto <108766073+Zarauto@users.noreply.github.com>
2023-09-12 10:21:21 -0300

    Update Main.cpp

--- Edubot/Main.cpp
+++ Edubot/Main.cpp
@@ -10,135 +10,35 @@
 #define SON_FRENTE 3	// Sonar frontal
 #define SON_NE 4		// Sonar nordeste
 
-#define DX 0.125		// Distância para o Edubot se mover a cada turno
-#define VEL 1		// Velocidade de movimento do Edubot
-#define OBST DX*1.5		// Distância para detecção de obstáculo
-#define RANGE_MAX 2 	// Máximo alcance do sonar
+#define SIMU 1
+#define OBST 20/pow(100,SIMU)
 #define WAIT 2000		// Tempo de espera para rotação e parada
 #define D_THETA 4		// Valor de tolerância para rotação
 #define CONT_SON 15		// Número de ticks para validar a leitura do sonar
+#define VEL 0.3
+#define DT_STEP 2000
 
 using namespace std;
 
-// Função que mede o tempo necessário para percorrer o DX na velocidade VEL
-// Executa uma vez no início do programa
-int medeDT(EdubotLib* edubot){
-
-	// Verifica cada direção para achar uma sem obstáculo e dentro do range de detecção
-	int i;
-	for(i = 0; i < 4; i++){
-		auto d = edubot->getSonar(SON_FRENTE);
-		if(d <= 2*DX || d >= RANGE_MAX){
-			edubot->rotate(-90);
-			edubot->sleepMilliseconds(WAIT);
-			continue;
-		}
-		break;
-	}
-
-	// Alterar para considerar caso em que começa no nada
-
-	// Acha distância inicial a obstáculo
-	auto d0 = edubot->getSonar(SON_FRENTE);
-
-	// Marca início do período de medição
-	auto ini = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch());
-
-	edubot->move(VEL);
-
-	// Percorre a distância 2dx
-	while(edubot->getSonar(SON_FRENTE) > d0 - DX);
-
-	// Marca fim do período de medição
-	auto fim = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch());
-	
-	edubot->neutral();
-	edubot->sleepMilliseconds(WAIT);
-
-	int dt = (fim-ini).count();
-
-	// Retorna Edubot para posição inicial
+void backStep(EdubotLib* edubot){
 	edubot->move(-VEL);
-	edubot->sleepMilliseconds(dt);
+	edubot->sleepMilliseconds(DT_STEP);
 	edubot->stop();
 	edubot->sleepMilliseconds(WAIT);
-	
-	return dt;
-	
 }
 
-// Faz o robô dar um passo
-void step(EdubotLib *edubot, int dt){
-	//if(DX> 
+void step(EdubotLib* edubot){
 	edubot->move(VEL);
-	edubot->sleepMilliseconds(dt);
-	edubot->stop();
-	edubot->sleepMilliseconds(WAIT);	
-	cout << "Dei um passo\n";
-}
-
-// Indica se o robô pode andar para a frente sem colidir
-bool frenteLivre(EdubotLib *edubot){
-	return edubot->getSonar(SON_FRENTE) > OBST;
-}
-
-// Garante que há uma parede à direita do robô
-bool verificaParede(EdubotLib *edubot){
-
-	for(int i=0; i<CONT_SON; i++){
-		if(edubot->getSonar(SON_DIR) > OBST){
-			return false;
-		}
-		edubot->sleepMilliseconds(10);
-	}
-
-	return true;	
-}
-
-// Verifica se o robô não percebe mais parede à direita
-bool verificaVazio(EdubotLib *edubot){
-
-	for(int i=0; i<CONT_SON; i++){
-		if(edubot->getSonar(SON_DIR) <= OBST){
-			return false;
-		}
-	}
-
-	return true;	
-}
-
-void andaFrente(EdubotLib *edubot, char interrupt = '0', char *motivo = NULL){
-	// Se houver um obstáculo, não anda
-	if(!frenteLivre(edubot)){
-		return;
-	}
-
-	// Anda até achar uma parede na frente ou ser interrompido
-	edubot->move(VEL);
-
-	while(frenteLivre(edubot)){
-		// Eventuais funções que interrompam o movimento
-
-		// Interrompe se achar parede à direita
-		if(interrupt == 'd' && verificaParede(edubot)){
-			edubot->stop();
-			edubot->sleepMilliseconds(WAIT);
-			*motivo = 'd';
-			return;
-		}
-
-		// Interrompe se parar de perceber parede à direita
-		if(interrupt == 'v' && verificaVazio(edubot)){
-			edubot->stop();
-			edubot->sleepMilliseconds(WAIT);
-			*motivo = 'v';
-			return;			
-		}
-	}
-
+	edubot->sleepMilliseconds(DT_STEP);
 	edubot->stop();
 	edubot->sleepMilliseconds(WAIT);
-	*motivo = 'f';
+}
+
+void halfStep(EdubotLib* edubot){
+	edubot->move(VEL);
+	edubot->sleepMilliseconds(DT_STEP/2);
+	edubot->stop();
+	edubot->sleepMilliseconds(WAIT);
 }
 
 // Normaliza um ângulo na faixa de -180 a 180 graus
@@ -181,7 +81,13 @@ void gira(EdubotLib *edubot, double ang){
 	// Enquanto estiver fora da faixa de tolerância,
 	// corrige a rotação
 	while(da > ang + D_THETA || da < ang - D_THETA){
-		double correcao = ang - da;
+		if(edubot->getBumper(0) or edubot->getBumper(1)){
+			backStep(edubot);
+		}
+		if(edubot->getBumper(2) or edubot->getBumper(3)){
+			halfStep(edubot);
+		}
+		double correcao = normAngulo(ang - da);
 		edubot->rotate(correcao);
 		edubot->sleepMilliseconds(WAIT);
 
@@ -190,134 +96,151 @@ void gira(EdubotLib *edubot, double ang){
 	}
 }
 
-// Acha uma parede à direita do robô
-void achaParede(EdubotLib *edubot){
+bool paredeNaFrente(EdubotLib *edubot){
+	double dist = 0; // Distância na frente do edubot
 
-	char dir;
+	// Lê a distância um número de vezes e faz a média
+	for(int i=0; i<CONT_SON;i++){
+		dist += edubot->getSonar(SON_FRENTE);
+	}
+	dist /=CONT_SON;
 
-	if(verificaParede(edubot)){
-		return;	
+	return dist <= OBST; // Se a média for menor ou igual ao limite de distância para ser considerado obstáculo, retorna verdadeiro
+}
+
+bool paredeNaEsquerda(EdubotLib *edubot){
+	double dist = 0; // Distância na frente do edubot
+
+	// Lê a distância um número de vezes e faz a média
+	for(int i=0; i<CONT_SON;i++){
+		dist += edubot->getSonar(SON_ESQ);
+	}
+	dist /=CONT_SON;
+
+	return dist <= OBST; // Se a média for menor ou igual ao limite de distância para ser considerado obstáculo, retorna verdadeiro
+}
+
+bool paredeNaDireita(EdubotLib *edubot){
+	double dist = 0; // Distância na frente do edubot
+
+	// Lê a distância um número de vezes e faz a média
+	for(int i=0; i<CONT_SON;i++){
+		dist += edubot->getSonar(SON_DIR	);
+	}
+	dist /=CONT_SON;
+
+	return dist <= OBST; // Se a média for menor ou igual ao limite de distância para ser considerado obstáculo, retorna verdadeiro
+}
+
+bool colidindoFrente(EdubotLib* edubot){
+	return edubot->getBumper(0) or edubot->getBumper(1);
+}
+
+int escolheLado(){
+	return rand()%2;
+}
+
+void viraAleatorio(EdubotLib* edubot){
+	gira(edubot, 90*pow(-1,escolheLado()));
+}
+
+void escolheCaminho(EdubotLib* edubot){
+
+	bool esq = paredeNaEsquerda(edubot), dir = paredeNaDireita(edubot), frente = paredeNaFrente(edubot);
+
+	// Beco sem saída
+	if(frente and esq and dir){
+		gira(edubot, 180);
+		//edubot->sleepMilliseconds(WAIT);
+		return;
 	}
 
-	andaFrente(edubot, 'd', &dir);
+	
+	// Só frente livre
+	if((not frente) and esq and dir){
+		return;
+	}
 
-	if(dir == 'f'){
-		gira(edubot,-90);
-	}	
-}
-
-void livraCaminho(EdubotLib *edubot, int dt){
-	gira(edubot, 90);
-
-	float d = edubot->getSonar(SON_NE);
-
-	gira(edubot, -90);
-
-	step(edubot, dt*d/DX);
-
-	gira(edubot,90);	
-}
-
-void maoDireita(EdubotLib *edubot, int dt){
-
-	while(true){
-	achaParede(edubot);
-	cout << "Achei uma parede\n";
-
-	char motivo;
-
-	andaFrente(edubot, 'v', &motivo);
-	cout << "Estou andando\n";
-
-	if(motivo == 'f'){
-		cout << "Parei porque tinha um obstáculo\n";
+	// Só esquerda livre
+	if((not esq) and frente and dir){
 		gira(edubot, -90);
-		//return;
-		continue;
+		return;
 	}
-	cout << "Parei porque tava vazio\n";
 
-	livraCaminho(edubot, dt);
-	
-	//andaFrente(edubot, 'd');
+	// Só direita livre
+	if((not dir) and frente and esq){
+		gira(edubot, 90);
+		return;
 	}
+
+	// Frente ou esquerda
+	if((not frente) and (not esq) and (dir)){
+		gira(edubot, -90*escolheLado());
+		return;
+	}
+
+	// Frente ou direita
+	if((not frente) and (esq) and (not dir)){
+		gira(edubot, 90*escolheLado());
+		return;
+	}
+
+	// Esquerda ou direita
+	if((frente) and (not esq) and (not dir)){
+		viraAleatorio(edubot);
+		return;
+	}
+
+	//Qualquer um
+	int lado = rand()%3;
+
+	if(lado == 0){
+		return;
+	}
+
+	viraAleatorio(edubot);
 }
 
-/*
-
-
-// Se a calibração do dt não pôde ser feita, acha outro ponto no mapa para calibrar
-void reposiciona(EdubotLib* edubot){
-
-	// Acha uma direção com caminho livre
-	for(int i = 0; i < 4; i++){
-		auto d = edubot->getSonar(SON_FRENTE);
-		if(d <= 3*DX){
-			edubot->rotate(-90);
-			edubot->sleepMilliseconds(WAIT);
-			continue;
-		}
-		break;
-	}
-
-	// Move até estar dentro da distância desejada
-	edubot->move(VEL);
-	while(edubot->getSonar(SON_FRENTE) > 3*DX);
-	edubot->stop();
-	edubot->sleepMilliseconds(WAIT);	
-}
-
-
-// Acha uma parede à direita do Edubot para ele seguir
-void achaParede(EdubotLib *edubot, bool *seguindoParede, int dt){
-
-	while(frenteLivre(edubot)){
-
-		cout << edubot->getSonar(SON_DIR) << endl;
-
-		
-		// Se achar uma parede à direita, retorna
-		if(edubot->getSonar(SON_DIR) <= OBST){
-			*seguindoParede = true;
-			return;		
-		}
-
-		step(edubot, dt);
-
-	}
-
-	// Se achar uma parede à frente, torna ela a direita e retorna
-	edubot->rotate(-90);
-	*seguindoParede = true;
-	return;	
-	
-}
-
-
-bool continuaParede(EdubotLib *edubot, bool deuVolta, int *voltas, int dt){
-	// Se já deu alguma volta seguindo essa parede e voltou à posição inicial,
-	// então abandona essa parede
-	if(deuVolta and *voltas == 0){
-		return false;
-	}
-
-	step(edubot, dt);
-	edubot->rotate(90);
+void passoLado(EdubotLib* edubot, int lado){
+	gira(edubot, 90*lado);
 	edubot->sleepMilliseconds(WAIT);
-	*voltas++;
-
-	step(edubot, 2*dt);
-
-	bool continua = edubot->getSonar(SON_DIR) <= OBST;
-	step(edubot, dt);
-	continua = continua && edubot->getSonar(SON_DIR) <= OBST;
-	cout << continua << endl;
-	return continua;
+	halfStep(edubot);
+	gira(edubot, -90*lado);
+	edubot->sleepMilliseconds(WAIT);
 	
 }
-*/
+
+void checaBumpers(EdubotLib* edubot){
+	bool bumpers[4];
+
+	for(int i=0;i<4;i++){
+		bumpers[i]=edubot->getBumper(i);
+	}
+
+	if(bumpers[0] or bumpers[1]){
+		edubot->stop();
+		edubot->sleepMilliseconds(WAIT);
+		backStep(edubot);
+
+		if(bumpers[0] and bumpers[1]){
+			return;
+		}
+
+		if(bumpers[0]){
+			passoLado(edubot,1);
+			return;
+		}
+
+		if(bumpers[1]){
+			passoLado(edubot,-1);
+		}
+		
+	}
+
+}
 
 int main(){
+	srand (time(NULL));
 
 	EdubotLib *edubot = new EdubotLib();
 
@@ -326,63 +249,16 @@ int main(){
 
 		edubot->sleepMilliseconds(WAIT);
 
-		/*
-		//andaPraFrente(edubot);
-
-		int dt = medeDT(edubot);
-		cout << dt << endl;
-
-		// Indica se o robô está seguindo uma parede ou procurando uma parede.
-		bool seguindoParede = false;
-
-		// Indica quantas vezes o robô girou
-		int voltas = 0;
-
-		// Auxiliar para determinar se o robô está seguindo uma parede desconexa
-		bool deuVolta = false;
-
-		
-		// Loop principal
 		while(true){
 
-			// Se o robô não estiver seguindo uma parede,
-			// acha uma parede para seguir
-			if(!seguindoParede){
-				achaParede(edubot, &seguindoParede, dt);
-				voltas = 0; // Zera o número de voltas
-				deuVolta = false;
-			}
-
-
-			// Se puder andar para a frente, dá um passo para a frente
-			if(frenteLivre(edubot)){
-				step(edubot,dt);
-			}
-
-			// Se estiver seguindo uma parede e for bloqueado, quer dizer que deve dobrar à esquerda
-			else{
-				edubot->rotate(-90);
-				edubot->sleepMilliseconds(WAIT);
-				voltas--;
-				deuVolta = true;
-			}
-
-			// Se não detectar parede à direita
-			if(edubot->getSonar(SON_DIR) > OBST){
-				seguindoParede = continuaParede(edubot, deuVolta, &voltas, dt);
-			}
-
-			//cout << "Voltas: " << voltas << endl;		
-
-			
-			
-		}*/
-
-		int dt = medeDT(edubot);
-		
-		while(true){
-			maoDireita(edubot, dt);
+			escolheCaminho(edubot);
+			edubot->sleepMilliseconds(WAIT);
+			step(edubot);			
+			checaBumpers(edubot);			
+	
 		}
+
+
 		edubot->disconnect();
 	}
 	else{
